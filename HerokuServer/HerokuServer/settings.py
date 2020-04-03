892549d19adbc80499d956855337e217717e0fbc
Fester2004 <61381794+Fester2004@users.noreply.github.com>
2020-04-03 12:31:40 +0300

    add some changes

--- HerokuServer/HerokuServer/settings.py
+++ HerokuServer/HerokuServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangodimon.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
